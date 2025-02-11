486004e30789837c8fa36857ef6b5bedb4f13f5a
anouther <155570107+anouther@users.noreply.github.com>
2025-02-11 17:30:26 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('Happy anniversary, Ayin!').split('');
+        const titles = ('Happy Valentine’s Day, my love.').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Every moment with you is an incredible blessing. Thank you for being my partner in life, full of love and understanding. I promise to always cherish and love you with all my heart, forever. May our love continue to grow and remain happy forever. I love you more than words can express.";
+            subtitle.innerText = "For the one who loves flowers more than anything, here’s a bouquet made just for you. Each petal is a reminder of how much you mean to me, blooming with love that will never fade. You’re my forever favorite, just like the flowers you adore.";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
